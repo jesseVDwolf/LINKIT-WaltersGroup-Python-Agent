4d8bf2cd3130c541476a9163ccdb607f1776d771
jesse.van.der.wolf <jesse.van.der.wolf@linkit.nl>
2023-01-13 13:19:02 +0100

    updated code

--- src/truck_agent/main.py
+++ src/truck_agent/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from typing import Tuple, List
 from .api import *
 
 app = FastAPI()
@@ -17,15 +18,19 @@ def decide(req: DecideRequest) -> DecideResponse:
         # 2.023 * liters_needed = price_for_gas
         diesel_price_per_liter = 2.023
         km_per_liter_consumption = 5.0
-        liters_needed: List[float] = [
+        cargo_delivery_time = 5.0
+        price_for_fuel: List[float] = [
             offer.km_to_deliver / km_per_liter_consumption * diesel_price_per_liter
             for offer in req.offers
         ]
 
+        def key_func(offer: Tuple[int, CargoOffer]) -> float:
+            return (offer[1].price - price_for_fuel[offer[0]])  / (offer[1].eta_to_deliver + cargo_delivery_time)
+
         # FIRST Algorithm
         # find the uid for the offer with the best margin where
         # margin = req.offers[x].price / req.offers[x].eta_to_deliver
-        _, offer = max(enumerate(req.offers), key=lambda offer: (offer[1].price - 0)  / offer[1].eta_to_deliver)
+        _, offer = max(enumerate(req.offers), key=key_func)
 
         return DecideResponse(command="DELIVER", argument=offer.uid)
     else:
