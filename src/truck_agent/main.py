e8e912716d386ef4710e20057c5625863e0b8af3
jesse.van.der.wolf <jesse.van.der.wolf@linkit.nl>
2023-01-13 13:52:26 +0100

    The driver may still have an incident, but WORTH IT

--- src/truck_agent/main.py
+++ src/truck_agent/main.py
@@ -19,16 +19,24 @@ def decide(req: DecideRequest) -> DecideResponse:
         diesel_price_per_liter = 2.023
         km_per_liter_consumption = 5.0
         cargo_delivery_time = 5.0
+        sleep_time = 24
 
         offers = [ {**cargo_offer.dict()} for cargo_offer in req.offers]
         for offer in offers:
             offer['price_for_fuel'] = offer['km_to_deliver'] / km_per_liter_consumption * diesel_price_per_liter
-            offer['gain'] = (offer['price'] - offer['price_for_fuel']) / (offer['eta_to_deliver'] + cargo_delivery_time)
+            incident_chance =  (offer['eta_to_deliver'] + req.truck.hours_since_full_rest - sleep_time) / 10
+            if incident_chance > 1:
+                incident_chance = 1
+            elif incident_chance < 0:
+                incident_chance = 0
+            offer['extra_time'] = incident_chance * 12.0
+            offer['gain'] = (offer['price'] - offer['price_for_fuel']) / (offer['eta_to_deliver'] + cargo_delivery_time + offer['extra_time'])
 
         # FIRST Algorithm
         # find the uid for the offer with the best margin where
         # margin = req.offers[x].price / req.offers[x].eta_to_deliver
         offer = max(offers, key=lambda x: x['gain'])
+
         if offer['gain'] < 0:
             return DecideResponse(command="SLEEP", argument=1)
 
