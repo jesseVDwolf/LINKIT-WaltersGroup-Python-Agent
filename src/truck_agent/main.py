1f04d80d53e0c69b701015e9e6814d8e43d701f1
jesse.van.der.wolf <jesse.van.der.wolf@linkit.nl>
2023-01-13 12:16:09 +0100

    Second algorithm

--- src/truck_agent/main.py
+++ src/truck_agent/main.py
@@ -12,10 +12,20 @@ def decide(req: DecideRequest) -> DecideResponse:
     """
     if req.offers:
 
+        # SECOND algorithm
+        # req.offers[x].km_to_deliver / km_per_liter_consumption = liters_needed
+        # 2.023 * liters_needed = price_for_gas
+        diesel_price_per_liter = 2.023
+        km_per_liter_consumption = 5.0
+        liters_needed: List[float] = [
+            offer.km_to_deliver / km_per_liter_consumption * diesel_price_per_liter
+            for offer in req.offers
+        ]
+
         # FIRST Algorithm
         # find the uid for the offer with the best margin where
         # margin = req.offers[x].price / req.offers[x].eta_to_deliver
-        offer = max(req.offers, key=lambda offer: offer.price / offer.eta_to_deliver)
+        _, offer = max(enumerate(req.offers), key=lambda offer: (offer[1].price - liters_needed[offer[0]])  / offer[1].eta_to_deliver)
 
         return DecideResponse(command="DELIVER", argument=offer.uid)
     else:
