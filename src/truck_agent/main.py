86dd0655902a3abfd55d393c25209e9fd99451d5
jesse.van.der.wolf <jesse.van.der.wolf@linkit.nl>
2023-01-13 16:06:10 +0100

    Last boundary change:

--- src/truck_agent/main.py
+++ src/truck_agent/main.py
@@ -21,7 +21,7 @@ def decide(req: DecideRequest) -> DecideResponse:
         cargo_delivery_time = 2.0
         sleep_time = 20
         start_time = 7
-        end_time = 17
+        end_time = 21
 
         offers = [ {**cargo_offer.dict()} for cargo_offer in req.offers]
         for offer in offers:
